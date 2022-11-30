a0883925a347682990b6bf3ab8e39c7946b4c43e
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-11-30 17:11:53 +0200

    added book class

---
+++ books.rb
@@ -0,0 +1,16 @@
+require_relative './item'
+
+class Book < Item
+  attr_reader :publisher, :cover_state, :archived
+
+  def initialize(publisher, cover_state, publish_date, archived)
+    super(publish_date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    true if super || cover_state == 'bad'
+    false
+  end
+end
