187ea791b9a0eb9c842c6f9e673ec73685cc0cb0
mule <belmeetmule@gmail.com>
2022-12-01 00:38:28 +0300

    fix linter errors

--- music_album.rb
+++ music_album.rb
@@ -1,15 +1,16 @@
 require './item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
+  attr_accessor :on_spotify
 
-    def initialize(on_spotify, publish_date, archived: false)
-        super(publish_date, archived: false)
-        @on_spotify = on_spotify
-    end
+  def initialize(on_spotify, publish_date, archived: false)
+    super(publish_date, archived: false)
+    @on_spotify = on_spotify
+  end
 
-    private
-    def can_be_archived?
-        super and @on_spotify
-      end
-    end
+  private
+
+  def can_be_archived?
+    super and @on_spotify
+  end
+end
