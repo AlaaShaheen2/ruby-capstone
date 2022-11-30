3e91a7fbaeec7ec17eee596dd56162156f9ce8e2
mule <belmeetmule@gmail.com>
2022-12-01 00:42:17 +0300

    update initilize

--- music_album.rb
+++ music_album.rb
@@ -4,7 +4,7 @@ class MusicAlbum < Item
   attr_accessor :on_spotify
 
   def initialize(on_spotify, publish_date, archived: false)
-    super(publish_date, archived: false)
+    super(publish_date, archived)
     @on_spotify = on_spotify
   end
 
