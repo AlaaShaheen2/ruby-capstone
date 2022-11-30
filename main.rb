c431693343156409c918a93a550e934b4748787e
mule <belmeetmule@gmail.com>
2022-11-30 16:50:52 +0300

    update user options

--- main.rb
+++ main.rb
@@ -2,15 +2,18 @@ require_relative './app'
 def menu
     @options = [
         '1 - List all books',
-        '2 - List all labels',
-        '3 - List all music albums',
-        '4 - List all genres',
-        '5 - List all games',
-        '6 - List all authors',
-        '7 - Add a book',
-        '8 - Add a music album',
-        '9 - Add a game',
-        '10 - Exit the App'
+        '2 - List all music albums',
+        '3 - List all movies',
+        '4 - List all games',
+        '5 - List all genres',
+        '6 - List all labels',
+        '7 - List all authors',
+        '8 - List all sources',
+        '9 - Add a book',
+        '10 - Add a music album',
+        '11 - Add a movie',
+        '12 - Add a game',
+        '13 - Exit the App'
         ]
 
     puts "\nPlease choose an option by entering a number:"
@@ -22,23 +25,29 @@ def option_case(choice, app)
     when 1
         app.list_all_books
       when 2
-        app.list_all_labels
-      when 3
         app.list_all_music_albums
+      when 3
+        app.list_all_movies
       when 4
-        app.list_all_genres
-      when 5
         app.list_all_games
+      when 5
+        app.list_all_genres
       when 6
-        app.list_all_authors
+        app.list_all_labels
     when 7
-        app.add_book
-    when 8
-        app.add_music_album
+        app.list_all_authors
+      when 8
+        app.list_all_sources
     when 9
+        app.add_book
+    when 10
+        app.add_music_album
+    when 11
+        app.add_movie
+    when 12
         app.add_game
     else
-      puts 'Incorrect choice, choose a number between 1..10'
+      puts 'Incorrect choice, choose a number between 1..11'
     end
   end
 
@@ -46,7 +55,7 @@ def option_case(choice, app)
     menu
     print "Enter choice:"
     choice = gets.chomp.to_i
-    choice == 10 ? app.quit_app : option_case(choice, app)
+    choice == 13 ? app.quit_app : option_case(choice, app)
   
     prompt_user (app)
   end
