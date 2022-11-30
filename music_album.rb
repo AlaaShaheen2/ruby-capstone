aaa72a334153425cc2d894d2e76adfa3402fbe34
mule <belmeetmule@gmail.com>
2022-12-01 02:42:48 +0300

    update getters and setters for the class

--- music_album.rb
+++ music_album.rb
@@ -1,10 +1,10 @@
 require './item'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify
+  attr_accessor :on_spotify, :label, :genre, :author
 
   def initialize(on_spotify, publish_date, archived: false)
-    super(publish_date, archived)
+    super(publish_date)
     @on_spotify = on_spotify
   end
 
