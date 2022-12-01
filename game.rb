38274ba7d38bf6b1345881e62a2bc27135928a7b
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-12-01 18:00:03 +0200

    fixed linter errorr

--- game.rb
+++ game.rb
@@ -4,8 +4,8 @@ require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played
 
-  def initialize(multiplayer, last_played, archived:false)
-    super(archived:archived)
+  def initialize(multiplayer, last_played, archived: false)
+    super(archived: archived)
     @multiplayer = multiplayer
     @last_played_date = Date.parse(last_played)
   end
