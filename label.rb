94c86069082beb5a2b84e97916150ceab0e16d78
mule <belmeetmule@gmail.com>
2022-12-02 22:40:10 +0300

    update label class

--- label.rb
+++ label.rb
@@ -1,10 +1,9 @@
-equire_relative './item'
-
 class Label
-  attr_reader :id, :title, :color, :items
+  attr_accessor :title, :color
+  attr_reader :id, :items
 
-  def initialize(_id, title, color)
-    @id = Random.rand(1..1000)
+  def initialize(title, color)
+    @id = Random.rand(1..200)
     @title = title
     @color = color
     @items = []
