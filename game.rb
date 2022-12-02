900cfc03a5c35cb56870347d08b0d44f22302fdc
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-12-02 17:28:52 +0200

    update game file

--- game.rb
+++ game.rb
@@ -2,16 +2,16 @@ require_relative './item'
 require 'date'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played
+  attr_accessor :multiplayer, :last_play
 
-  def initialize(multiplayer, last_played, archived: false)
+  def initialize(multiplayer, _last_played, archived: false)
     super(archived: archived)
     @multiplayer = multiplayer
-    @last_played_date = Date.parse(last_played)
+    @last_played_date = Date.parse(last_play)
   end
 
   def can_be_archived?
-    test = Time.now.year - @last_played.year > 2
+    test = Time.now.year - @last_play.year > 2
     archived && test
   end
 end
