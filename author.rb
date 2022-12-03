05c5b36307a93db2547787aa4918a8ddec5753ec
mule <belmeetmule@gmail.com>
2022-12-03 15:03:26 +0300

    update to read from a file

--- author.rb
+++ author.rb
@@ -1,7 +1,7 @@
 class Author
   # getters and setters
-  attr_reader :id, :items
-  attr_accessor :first_name, :last_name
+  attr_reader  :items
+  attr_accessor :first_name, :last_name, :id
 
   def initialize(first_name, last_name)
     @id = Random.rand(1..200)
